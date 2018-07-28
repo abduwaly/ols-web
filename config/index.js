8507e3d64a10d4d2a29951dea6f5a9fd23d9ce82
Shihao Gao <shihao.gao@rea-group.com>
2018-07-29 00:03:53 +0800

    feat: Add CI Scripts & Jenkins Pipeline

--- config/index.js
+++ config/index.js
@@ -6,14 +6,18 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api/*':{
+        target:'http://127.0.0.1:8081/',
+        changeOrigin:true,
+      }
+    },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
