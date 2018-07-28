687c5ceee9aa8c43480babcae434a23cbb40547c
Hongxing Chen <honxchen@CNhonxchen.local>
2018-07-28 21:47:39 +0800

    check username and password

--- src/components/SignUp/index.vue
+++ src/components/SignUp/index.vue
@@ -1,36 +1,29 @@
 <template>
-  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px">
+
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="signup">
+    <div class="signup-header">
+      <h1>思沃学院</h1>
+    </div>
     <el-form-item label="用户名：" prop="userName">
       <el-col :span="12">
-        <el-input v-model="ruleForm.userName"></el-input>
+        <el-input v-model="ruleForm.userName" placeholder="必须是1-10个英文字符，不能有空格"></el-input>
       </el-col>
     </el-form-item>
     <el-form-item label="密码：" prop="password">
       <el-col :span="12">
-        <el-input type="password" v-model="ruleForm.password"></el-input>
+        <el-input type="password" v-model="ruleForm.password" placeholder="必须是大于8个的数字，不能有空格"></el-input>
       </el-col>
     </el-form-item>
-    <el-form-item label="确认密码：" prop="password2">
-      <el-col :span="12">
-        <el-input type="password" v-model="ruleForm.password2"></el-input>
-      </el-col>
-    </el-form-item>
-    <el-form-item label="验证码：" prop="capture">
-      <el-col :span="12">
-        <el-input v-model="ruleForm.capture"></el-input>
-      </el-col>
-      <el-col :span="6">
-        <el-button>刷新</el-button>
-      </el-col>
-    </el-form-item>
+
     <el-form-item>
-      <el-col :span="1">
-        <el-button type="primary" @click="submitForm('ruleForm')">注册</el-button>
+      <el-col :span="12">
+        <el-button type="primary" @click="submitForm('ruleForm')" class="signup-button">注册</el-button>
       </el-col>
     </el-form-item>
-    <el-form-item>
-      <el-col :span="1">
-        <router-link to="/">返回</router-link>
+    <el-form-item class="signup-login">
+      <el-col >
+        <span>已注册？</span>
+        <router-link to="/">登陆</router-link>
       </el-col>
     </el-form-item>
   </el-form>
@@ -41,26 +34,35 @@
 
   export default {
     data() {
+      var validateUser = (rule, value, cb) => {
+        var pattern = '/^[a-zA-Z]{1,10}$/g';
+        if (value === '') {
+          cb(new Error('请输入用户名'))
+        } else if (!pattern.test(value)) {
+          cb(new Error())
+        } else {
+          cb()
+        }
+      }
+      var validatePwd = (rule, value, cb) => {
+        var pattern = '/^[0-9]{9,}$/g';
+        if (!pattern.test(value)) {
+          cb(new Error())
+        }else{
+          cb();
+        }
+      }
       return {
         ruleForm: {
           userName: '',
           password: '',
-          password2: '',
-          capture: '',
         },
         rules: {
           userName: [
-            {required: true, message: '请输入用户名', trigger: 'blur'},
-            {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']}
+            {validator: validateUser ,message: '必须是1-10个英文字符，不能有空格', trigger: 'blur'},
           ],
           password: [
-            {required: true, message: '请输入密码', trigger: 'blur'}
-          ],
-          password2: [
-            {required: true, message: '请确认密码', trigger: 'blur'}
-          ],
-          capture: [
-            {required: true, message: '请输入验证码', trigger: 'blur'}
+            { validator: validatePwd, message: '必须是大于8个的数字，不能有空格', trigger: 'blur'}
           ]
         }
       }
@@ -72,3 +74,25 @@
     }
   }
 </script>
+
+<style scoped>
+  .signup{
+    width: 50%;
+    margin-left: 25%;
+    text-align: center;
+  }
+
+  .signup .signup-header{
+    border-bottom: gainsboro solid 1px;
+    margin-bottom: 20px;
+  }
+
+  .signup .signup-button{
+    width:100%;
+    background-color: dodgerblue;
+  }
+
+  .signup .signup-login{
+    text-align: left;
+  }
+</style>
