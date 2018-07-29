d896ec1e4eda8431b96d9f6b1f0abfa85a08515e
abduwaly <arsay@foxmail.com>
2018-07-29 09:02:50 +0800

    init Sign In page UI

--- src/components/SignIn/index.vue
+++ src/components/SignIn/index.vue
@@ -1,35 +1,59 @@
+<style>
+  .page-content{
+    width: 500px;
+    margin: 0 auto;
+  }
+  .header-title{
+    font-size: 50px;
+    text-align: center;
+    padding: 25px 0;
+    margin: 30px 0;
+    border-bottom: #ccc .5px solid;
+  }
+  .login-form{
+    margin-top: 20px;
+  }
+  .el-col{
+    transform: translateX(-60px);
+  }
+  .login-button{
+    width: 100%;
+    background-color: #0178bb;
+    border-color: #0178bb;
+  }
+  .center{
+    text-align: center;
+  }
+</style>
 <template>
-  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px">
-    <el-form-item label="用户名：" prop="userName">
-      <el-col :span="12">
-        <el-input v-model="ruleForm.userName"></el-input>
-      </el-col>
-    </el-form-item>
-    <el-form-item label="密码：" prop="password">
-      <el-col :span="12">
-        <el-input type="password" v-model="ruleForm.password"></el-input>
-      </el-col>
-    </el-form-item>
-    <el-form-item label="验证码：" prop="capture">
-      <el-col :span="12">
-        <el-input v-model="ruleForm.capture"></el-input>
-      </el-col>
-      <el-col :span="6">
-        <el-button>刷新</el-button>
-      </el-col>
-    </el-form-item>
-    <el-form-item>
-      <el-col :span="1">
-        <el-button type="primary">登陆</el-button>
-      </el-col>
-    </el-form-item>
-    <el-form-item>
-      <el-col :span="1">
-        <router-link to="/signup">注册</router-link>
-        <el-button type="primary" @click="submitForm('signupForm')">登陆</el-button>
-      </el-col>
-    </el-form-item>
-  </el-form>
+  <div class="page-content">
+    <div class="login-header">
+      <p class="header-title">思沃学院</p>
+    </div>
+    <el-form class="login-form" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px">
+      <h2 class="center">Sign In</h2>
+      <el-form-item prop="userName">
+        <el-col :span="24" class="el-col">
+          <el-input v-model="ruleForm.userName" placeholder="Username"></el-input>
+        </el-col>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-col :span="24" class="el-col">
+          <el-input type="password" v-model="ruleForm.password" placeholder="Password"></el-input>
+        </el-col>
+      </el-form-item>
+      <el-form-item>
+        <el-col :span="24" class="el-col">
+          <el-button class="login-button" type="primary" @click="submitForm('ruleForm')">Sign In</el-button>
+        </el-col>
+      </el-form-item>
+      <el-form-item>
+        <el-col :span="24" class="el-col">
+           新用户？ <router-link to="/signup">注册</router-link>
+        </el-col>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
